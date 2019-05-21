fc7a6336c6a7dc5f7bcd3cab7ae5f2f98266bfae
Zhifei Yang <zhiyang@student.ethz.ch>
2019-05-21 16:46:27 +0200

    another aligned malloc/free for reference

--- include/lambdamart/lambdamart.h
+++ include/lambdamart/lambdamart.h
@@ -23,6 +23,25 @@ namespace LambdaMART {
     const char* help() {
         return "usage: lambdamart config_file";
     }
+
+    inline void* aligned_malloc(size_t size, size_t align) {
+        void *result;
+#ifdef _MSC_VER
+        result = _aligned_malloc(size, align);
+#else
+        if(posix_memalign(&result, align, size)) result = 0;
+#endif
+        return result;
+    }
+
+    inline void aligned_free(void *ptr) {
+#ifdef _MSC_VER
+        _aligned_free(ptr);
+#else
+        free(ptr);
+#endif
+
+    }
 }
 
 #endif //LAMBDAMART_LAMBDAMART_H
